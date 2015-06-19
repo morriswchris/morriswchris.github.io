7eeb4742f5a929993403bd5a5a6d3662101e7787
Chris Morris <morris.w.chris@gmail.com>
2015-06-18 23:05:35 -0400

    updated site

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,9 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-        $("#home").removeClass("hidden");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        $("#home").addClass("hidden");
+$("#command").typed({
+    strings: ["which morriswchris", "whois morriswchris", "morisw", "morriswchris -v", "less ~/repo/morriswchris/about.md" ],
+    showCursor: false,
+    typeSpeed: 20,
+    callback: function() {
+        $("#terminal").toggle()
+        $("#output").toggle();
     }
-});
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
 });
