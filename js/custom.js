e42b00a5f8b37df709378486ada64d8c17433cce
Chris Morris <morriswchris@gmail.com>
2020-08-16 13:59:48 -0400

    update site

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $("#command").typed({
     strings: ["which morriswchris", "whois morriswchris", "less ~/repo/morriswchris/about.md" ],
     showCursor: false,
-    typeSpeed: 20,
+    typeSpeed: 25,
     callback: function() {
         $("#terminal").toggle()
         $("#output").toggle();
